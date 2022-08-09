6970590c8591745becb2d04cce3ca98817675b2a
Alex Herrera <38311783+AlexHerrerax@users.noreply.github.com>
2022-08-09 04:51:07 -0400

    Actualizacion

--- callback/app.js
+++ callback/app.js
@@ -1,1 +1,34 @@
-console.log("Callback")
+const personajes = {
+  capi: { nombre: "Capitan America", poder: "Escudo" },
+  iron: { nombre: "Ironman", poder: "Dinero" },
+  spider: { nombre: "Spiderman", poder: "Telaraña" },
+};
+
+const heroeId = "capi"
+
+//Funcion
+const buscarHeroe = (id, callback) =>{
+
+    const heroe = personajes[id]
+
+    if(heroe){
+        callback(null, heroe)
+    }else{
+        //Error
+        callback(`El id del heroe ${heroe} no existe`) 
+    }
+    
+
+}
+
+
+
+//Llamado de la funcion
+
+buscarHeroe(heroeId, (error, heroe) =>{
+    if(error){
+        console.error(error)
+    }else{
+        console.log(heroe)
+    }
+})
