df0ce2d055ee72e006478cf40b26113b5e589152
Alex Herrera <38311783+AlexHerrerax@users.noreply.github.com>
2022-08-09 05:35:12 -0400

    Actualizacion

--- promesas/app.js
+++ promesas/app.js
@@ -1,1 +1,28 @@
-console.log("promesas")
+const personajes = {
+  capi: { nombre: "Capitan America", poder: "Escudo" },
+  iron: { nombre: "Ironman", poder: "Dinero" },
+  spider: { nombre: "Spiderman", poder: "Telaraña" },
+};
+
+const heroeId = "capi";
+
+const promesas = (id) => {
+    
+  const heroe = personajes[id];
+
+  return new Promise((resolve, reject) => {
+    if (heroe) {
+      resolve(heroe);
+    } else {
+      reject(`No existe un heroe con el id ${heroe}`);
+    }
+  });
+};
+
+promesas(heroeId).then((heroe)=>{
+    console.log(`Enviando a ${heroe.nombre} a la mision`)
+})
+
+
+
+
